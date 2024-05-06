b043425e271a1b2f2623f4abf01f277aae2fe1a9
Dapo Paul Ogunlana <dapoogunlana@gmail.com>
2024-05-06 20:35:54 +0100

    flight flow pages added

---
+++ src/pages/user/flights/flight-booking/flight-booking.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './flight-booking.scss';
+
+function FlightBookingPage() {
+  
+  return (
+    <div className='flight-booking'>
+      <div className='text'>
+        <h1 className='f700'>Borderless</h1>
+        <h3><span className='yellow-tx'>Seamless </span>booking,</h3>
+        <h3><span className='orange-tx'>boundless </span>adventures</h3>
+      </div>
+    </div>
+  );
+}
+
+export default FlightBookingPage;
---
+++ src/pages/user/flights/flight-preview/flight-preview.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './flight-preview.scss';
+
+function FlightPreviewPage() {
+  
+  return (
+    <div className='flight-preview'>
+      <div className='text'>
+        <h1 className='f700'>Borderless</h1>
+        <h3><span className='yellow-tx'>Seamless </span>booking,</h3>
+        <h3><span className='orange-tx'>boundless </span>adventures</h3>
+      </div>
+    </div>
+  );
+}
+
+export default FlightPreviewPage;
